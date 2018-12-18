cacf4925045eb9e0a618431806dab16bc09283c9
synthetic <syntheticintelligence0@gmail.com>
2018-12-18 14:59:59 +0530

    automated sh script

--- auto.sh
+++ auto.sh
@@ -1,16 +1,8 @@
-#!/bin/sh
-git bash
+
+#!/bin/bash
+
 git init
 git add .
 
-echo 'Enter the commit message:'
-read commitMessage 
-
-git commit -m "$commitMessage"
-
-echo 'Enter the name of the branch:'
-read branch
-
-git push origin $branch
-
-read
+git commit -m"automated sh script"
+git push -u origin master
